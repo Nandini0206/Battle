9731dfb2ac3a25657a808eb8fb39e1ad2b0b3e86
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-04-24 20:43:19 +0100

    use web-helper to refactor and keep code DRY

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter name' do
   scenario 'add names and then submit' do
-    visit('/')
-    fill_in :player_1_name, with: 'Nandini'
-    fill_in :player_2_name, with: 'Tara'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Nandini vs Tara'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'view player 2 hitpoints' do
-    visit '/'
-    fill_in :player_1_name, with: 'Nandini'
-    fill_in :player_2_name, with: 'Tara'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Tara: 60HP'
   end
 end
